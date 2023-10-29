7561c819273540e74702a80d5deeef551a5b153b
MiguelSanLeon <>
2023-10-29 15:59:48 +0000

    Add html files to templates folder

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Booking
+
 
 # Create your views here.
+
+class HomePage(View):
+    template_name = "index.html"
+
+    def get(serf, request, *args, **kwargs):
+        return render(request, "index.html")
